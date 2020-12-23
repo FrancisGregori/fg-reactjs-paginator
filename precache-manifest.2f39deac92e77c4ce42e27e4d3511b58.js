9da559eacb3fafddc723aafb283bd888e877adfa
Francis Gregori <francis.g.munis@gmail.com>
2020-12-22 23:19:26 -0300

    Updates

---
+++ precache-manifest.2f39deac92e77c4ce42e27e4d3511b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63b4bb8b53d59433ebd4e0537dcc35e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8bb3dcb8e45400d3216",
+    "url": "./static/css/main.9b636161.chunk.css"
+  },
+  {
+    "revision": "fcc9ed80349cb775b27e",
+    "url": "./static/js/2.02aac57a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.02aac57a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8bb3dcb8e45400d3216",
+    "url": "./static/js/main.edadc179.chunk.js"
+  },
+  {
+    "revision": "7b1667925b59715573ad",
+    "url": "./static/js/runtime-main.12d8ff40.js"
+  }
+]);
